b365e439fa6ffb9a8e0ef9a5e48dc63ce4cf800e
znxingbell <znxingbell@gmail.com>
2025-02-26 21:37:05 +0800

    init

---
+++ src/main/java/com/znxingbell/carpetartaddition/CarpetARTAdditionClient.java
@@ -0,0 +1,11 @@
+package com.znxingbell.carpetartaddition;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CarpetARTAdditionClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/com/znxingbell/carpetartaddition/CarpetARTAdditionDataGenerator.java
@@ -0,0 +1,11 @@
+package com.znxingbell.carpetartaddition;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CarpetARTAdditionDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
